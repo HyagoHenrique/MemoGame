bf4143f6cc255a648a32f8592952698f28940e96
Hyago <hyagoh@icloud.com>
2021-07-12 13:02:58 -0300

    Initial Commit

---
+++ MemoGame/MemoGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoGameApp.swift
+//  MemoGame
+//
+//  Created by Hyago Henrique on 12/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MemoGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
